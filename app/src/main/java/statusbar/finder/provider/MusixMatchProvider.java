26105aaf519d6e4e6a88b4afa4ad7f9dc4f8e6e1
VictorModi <vm@vm.mba>
2023-09-07 04:29:16 +0800

    将MusixMatch源调整为它应该的样子

--- app/src/main/java/statusbar/finder/provider/MusixMatchProvider.java
+++ app/src/main/java/statusbar/finder/provider/MusixMatchProvider.java
@@ -1,9 +1,11 @@
 package statusbar.finder.provider;
 
 import android.media.MediaMetadata;
+import android.util.Pair;
 
 import com.github.houbb.opencc4j.util.ZhConverterUtil;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -11,53 +13,64 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.lang.NullPointerException;
 import java.net.URLEncoder;
+import java.util.Locale;
 
 import statusbar.finder.misc.checkStringLang;
 import statusbar.finder.provider.utils.HttpRequestUtil;
+import statusbar.finder.provider.utils.LyricSearchUtil;
 
 public class MusixMatchProvider implements ILrcProvider {
 
     private static final String MUSIXMATCH_BASE_URL = "https://apic.musixmatch.com/ws/1.1/";
     private static final String MUSIXMATCH_TOKEN_URL_FORMAT = MUSIXMATCH_BASE_URL + "token.get?guid=%s&app_id=android-player-v1.0&format=json";
-    private static final String MUSIXMATCH_LRC_URL_FORMAT = MUSIXMATCH_BASE_URL + "macro.subtitles.get?tags=playing&subtitle_format=lrc&usertoken=%s&q_track=%s&q_artist=%s&q_album=%s&app_id=android-player-v1.0&format=json";
-
+    // private static final String MUSIXMATCH_LRC_URL_FORMAT = MUSIXMATCH_BASE_URL + "macro.subtitles.get?tags=playing&subtitle_format=lrc&usertoken=%s&q_track=%s&q_artist=%s&q_album=%s&app_id=android-player-v1.0&format=json";
+    private static final String MUSIXMATCH_LRC_URL_FORMAT = MUSIXMATCH_BASE_URL + "macro.subtitles.get?tags=playing&subtitle_format=lrc&usertoken=%s&track_id=%s&app_id=android-player-v1.0&format=json";
+    private static final String MUSIXMATCH_SERACH_URL_FORMAT = MUSIXMATCH_BASE_URL + "macro.search?app_id=android-player-v1.0&usertoken=%s&q=%s";
     private static String MUSIXMATCH_USERTOKEN;
     @Override
     public LyricResult getLyric(MediaMetadata data) throws IOException {
-        JSONObject lrcFullJson;
-        String lrcUrl;
         if (MUSIXMATCH_USERTOKEN  == null) {
             MUSIXMATCH_USERTOKEN = getMusixMatchUserToken("");
             if (MUSIXMATCH_USERTOKEN  == null) {
                 return null;
             }
         }
+        String searchUrl = String.format(MUSIXMATCH_SERACH_URL_FORMAT, MUSIXMATCH_USERTOKEN , LyricSearchUtil.getSearchKey(data));
+        JSONObject searchResult;
         try{
-            String track = toSimpleURLEncode(data.getString(MediaMetadata.METADATA_KEY_TITLE));
-            String artist = toSimpleURLEncode(data.getString(MediaMetadata.METADATA_KEY_ARTIST));
-            String album = toSimpleURLEncode(data.getString(MediaMetadata.METADATA_KEY_ALBUM));
-            lrcUrl = String.format(MUSIXMATCH_LRC_URL_FORMAT,
-                    MUSIXMATCH_USERTOKEN,
-                    track,
-                    artist,
-                    album);
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-            return null;
-        }
-        try{
-            LyricResult result = new LyricResult();
-            JSONObject subTitleJson;
-            lrcFullJson = HttpRequestUtil.getJsonResponse(lrcUrl);
-            subTitleJson = lrcFullJson.getJSONObject("message").getJSONObject("body").getJSONObject("macro_calls").getJSONObject("track.subtitles.get").getJSONObject("message").getJSONObject("body").getJSONArray("subtitle_list").getJSONObject(0).getJSONObject("subtitle");
-            result.mLyric = subTitleJson.getString("subtitle_body");
-            result.mDistance = subTitleJson.getLong("subtitle_length") * 1000;
-            result.source = "MusixMatch";
-            return result;
+            searchResult = HttpRequestUtil.getJsonResponse(searchUrl);
+            if (searchResult != null && searchResult.getJSONObject("message").getJSONObject("header").getLong("status_code") == 200) {
+                JSONArray array = searchResult.getJSONObject("message").getJSONObject("body").getJSONObject("macro_result_list").getJSONArray("track_list");
+                Pair<String, Long> pair = getLrcUrl(array, data);
+                JSONObject lrcJson = HttpRequestUtil.getJsonResponse(pair.first);
+                LyricResult result = new LyricResult();
+                result.mLyric = lrcJson.getJSONObject("message").getJSONObject("body").getJSONObject("macro_calls").getJSONObject("track.subtitles.get").getJSONObject("message").getJSONObject("body").getJSONArray("subtitle_list").getJSONObject(0).getJSONObject("subtitle").getString("subtitle_body");
+                result.mDistance = pair.second;
+                result.source = "MusixMatch";
+                return result;
+            }
         } catch (JSONException e) {
             e.printStackTrace();
             return null;
         }
+        return null;
+    }
+
+    private static Pair<String, Long> getLrcUrl(JSONArray jsonArray, MediaMetadata mediaMetadata) throws JSONException {
+        long currentID = -1;
+        long minDistance = 10000;
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i).getJSONObject("track");
+            String soundName = jsonObject.getString("track_name");
+            String albumName = jsonObject.getString("album_name");
+            String artists = jsonObject.getString("artist_name");
+            long dis = LyricSearchUtil.getMetadataDistance(mediaMetadata, soundName, artists, albumName);
+            if (dis < minDistance) {
+                minDistance = dis;
+                currentID = jsonObject.getLong("track_id");
+            }
+        }
+        return new Pair<>(String.format(Locale.getDefault(), MUSIXMATCH_LRC_URL_FORMAT, MUSIXMATCH_USERTOKEN, currentID), minDistance);
     }
 
     private String toSimpleURLEncode(String input) {
